97a3c36a329fca02f94125b3a4eedecfaa7d397d
LEANDRO CARVALHO BATISTA <leandrocb@alunofg.com.br>
2018-10-14 22:56:44 -0300

    Foi criado um mapeamento do objeto Usuario.

--- ProjetoSD/ProjetoSD.WebAPI/DAO/EntidadeContext.cs
+++ ProjetoSD/ProjetoSD.WebAPI/DAO/EntidadeContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using ProjetoSD.WebAPI.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,6 +13,8 @@ namespace ProjetoSD.WebAPI.DAO
         private string StringConnectionSqlServer = string.Format(@"Server=DESKTOP-R3HGIEK;Database=ProjetoSD;User Id=sa;
 Password=@Leandro123;");
 
+        public DbSet<Usuario> Usuarios { get; set; }
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer(StringConnectionSqlServer);
@@ -19,7 +22,23 @@ Password=@Leandro123;");
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            
+            #region Usuario - TUSU
+            modelBuilder.Entity<Usuario>().ToTable("TUSU");
+
+            modelBuilder.Entity<Usuario>().Property(u => u.Id).HasColumnName("CUSUID");
+
+            modelBuilder.Entity<Usuario>().Property(u => u.Email).HasColumnName("CUSUEMAIL");
+            modelBuilder.Entity<Usuario>().Property(u => u.Email).HasMaxLength(60);
+            modelBuilder.Entity<Usuario>().Property(u => u.Email).IsRequired();
+
+            modelBuilder.Entity<Usuario>().Property(u => u.Senha).HasColumnName("CUSUSENHA");
+            modelBuilder.Entity<Usuario>().Property(u => u.Senha).HasMaxLength(25);
+            modelBuilder.Entity<Usuario>().Property(u => u.Senha).IsRequired();
+
+            modelBuilder.Entity<Usuario>().Property(u => u.TipoStatus).HasColumnName("CUSUTIPOSTATUS");
+            modelBuilder.Entity<Usuario>().Property(u => u.TipoStatus).HasConversion(u => u.ToString(), u => (TipoStatus)Enum.Parse(typeof(TipoStatus), u));
+            modelBuilder.Entity<Usuario>().Property(u => u.TipoStatus).HasDefaultValue(TipoStatus.S);
+            #endregion
         }
     }
 }
---
+++ ProjetoSD/ProjetoSD.WebAPI/Models/Usuario.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProjetoSD.WebAPI.Models
+{
+    public class Usuario
+    {
+
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string Senha { get; set; }
+        public TipoStatus TipoStatus { get; set; }
+
+    }
+    
+    public enum TipoStatus
+    {
+        N, S
+    }
+}
