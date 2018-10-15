e087131ebbf1edf4fe45f4317421e0565d853b60
LEANDRO CARVALHO BATISTA <leandrocb@alunofg.com.br>
2018-10-14 21:45:32 -0300

    Foi criado estrutura para poder receber entity framework core.

---
+++ ProjetoSD/ProjetoSD.WebAPI/DAO/EntidadeContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProjetoSD.WebAPI.DAO
+{
+    public class EntidadeContext : DbContext
+    {
+
+        private string StringConnectionSqlServer = string.Format(@"Server=DESKTOP-R3HGIEK;Database=ProjetoSD;User Id=sa;
+Password=@Leandro123;");
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(StringConnectionSqlServer);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            
+        }
+    }
+}
