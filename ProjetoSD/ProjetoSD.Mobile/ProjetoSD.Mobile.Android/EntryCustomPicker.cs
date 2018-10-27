f5383ff317f4f10ee8bc8a9d81853000483db122
LEANDRO CARVALHO BATISTA <leandrocb@alunofg.com.br>
2018-10-27 14:27:20 -0300

    Customização dos componentes entry e picker.

---
+++ ProjetoSD/ProjetoSD.Mobile/ProjetoSD.Mobile.Android/EntryCustomPicker.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.Content.Res;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using ProjetoSD.Mobile.Droid;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer (typeof(Entry), typeof(EntryCustomPicker))]
+namespace ProjetoSD.Mobile.Droid
+{
+    public class EntryCustomPicker : EntryRenderer
+    {
+        public EntryCustomPicker(Context context) : base(context)
+        {
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+            Control.BackgroundTintList = ColorStateList.ValueOf(Android.Graphics.Color.White);
+            Control.SetHintTextColor(ColorStateList.ValueOf(Android.Graphics.Color.White));
+        }
+    }
+}
---
+++ ProjetoSD/ProjetoSD.Mobile/ProjetoSD.Mobile.Android/PickerCustomRenderer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.Content.Res;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using ProjetoSD.Mobile.Droid;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(Picker), typeof(PickerCustomRenderer))]
+namespace ProjetoSD.Mobile.Droid
+{    
+    public class PickerCustomRenderer : PickerRenderer
+    {
+        public PickerCustomRenderer(Context context) : base(context)
+        {
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Picker> e)
+        {
+            base.OnElementChanged(e);
+            Control.BackgroundTintList = ColorStateList.ValueOf(Android.Graphics.Color.White);
+            Control.SetHintTextColor(ColorStateList.ValueOf(Android.Graphics.Color.White));
+        }
+    }
+}
